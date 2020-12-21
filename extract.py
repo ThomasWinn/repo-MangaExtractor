1bab111ad636596a8cc5f39c66471195015b3b3e
Thomas Nguyen <tpnguyen1999@gmail.com>
2020-12-21 02:05:10 -0600

    Extracted img from website so far

--- extract.py
+++ extract.py
@@ -1,70 +1,102 @@
 # pip install requests
 # pip install html5lib
 # pip install bs4
-# pip install google 
 # pip install manganelo
+# pip install fpdf
 
 import requests
 from bs4 import BeautifulSoup
-# import google 
+import urllib.parse 
 from manganelo import SearchManga
 from manganelo import MangaInfo
-import csv
+from PyPDF2 import PdfFileReader
+import os
+from PIL import Image
 
+def get_manga_panels(url):
+    # get manga panels
+    panels = []
 
-if __name__ == '__main__':
-    print("hello")
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
 
-    search = SearchManga("Grand Blue", threaded=True)
+    div_imgs = soup.find('div', attrs={'class':'container-chapter-reader'})
+
+    for i in div_imgs.findAll('img'):
+        panels.append(i['src'])
+
+    return panels
+
+def extract_manga_chapter_url(url, chapter):
+    parsed_url = url.split('/')
+
+    # get chapter url
+    url_part = list(urllib.parse.urlparse(url))
+    url_part[2] = '/chapter/{}/chapter_{}'.format(parsed_url[4], chapter)
+    manga_url_chapter = urllib.parse.urlunparse(url_part)
+
+    return manga_url_chapter
+
+def main():
+    # ADD USER INPUT
+    # title = input("Manga to Download: ")
+    # download_all = input("Download all Chapters? YES OR NO: ")
+    # chapter = input("Chapter to Download: ")
+
+    # search = SearchManga(title, threaded=True)
+
+    # COMMENT WHEN DONE FOR TESTING ONLY
+    search = SearchManga("boruto", threaded=True)
+    chapter = 1
 
     # .results() returns a generator - We create a list from the generator here
     results = list(search.results())
 
-    print(results)
+    best_result = results[0] # grand blue
 
-    best_result = results[0]
+    manga_title = best_result.title
+    manga_url_homepage = best_result.url
 
-    manga_info = MangaInfo(best_result.url, threaded=True)
-
-    manga_page = manga_info.results()
-
-    print()
-    print(manga_page.chapters)
+    manga_url_chapter = extract_manga_chapter_url(manga_url_homepage, chapter)
 
 
-    # results = [MangaSearchResult(title=?, url=?), MangaSearchResult(title=?, url=?)]
+    panel_list = get_manga_panels(manga_url_chapter) # get all images from chapter
+
+    PIL_list = []
+    for i in panel_list:
+        img = Image.open(i)
+        PIL_list.append(img)
+
+    pdf_fname = 'chapter_{}.pdf'.format(chapter)
+
+    PIL_list[0].save(pdf_fname, "PDF", resolution=100.0, save_all=True, append_images=PIL_list)
+    # Download Manga images to pdf
 
 
+    '''
+    Check cwd for 'Manga' folder -> if none, make one else do nothing
+
+    Make folder for title of manga to download -> if none, make one else do nothing
+
+    def download
+        create folder chapter_NUM
+        open pdf
+        add jpg images on to it
+        center preferred
+    '''
 
 
+if __name__ == '__main__':
+
+    main()
 
 
-
-
-
-
-
-
-    # # prints all the html elements CHOPPY UGLY
-    # URL = "https://www.geeksforgeeks.org/data-structures/"
-    # r = requests.get(URL)
-    # print(r.content)
-
-
-    # GETS ALL HTML TAGS BUT PRETTY
-    # URL = "http://www.values.com/inspirational-quotes"
-    # r = requests.get(URL) 
-    
-    # soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
-    # print(soup.prettify()) 
-
-    # EXPORT TO CSV
+    # # EXPORT TO CSV
     # URL = "http://www.values.com/inspirational-quotes"
     # r = requests.get(URL) 
     
     # soup = BeautifulSoup(r.content, 'html5lib') 
-    # print(soup.prettify()) 
-    
+
     # quotes=[]  # a list to store quotes 
     
     # table = soup.find('div', attrs = {'id':'all_quotes'})  
